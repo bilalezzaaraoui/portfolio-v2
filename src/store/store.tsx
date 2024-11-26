37dc4f8c39d33f897ec1af7044328c59ff0a80f2
bilal <bilalezzaa1@gmail.com>
2024-11-26 16:36:11 +0100

    Fix du store 2ème essaie

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,11 +1,17 @@
 import { create } from 'zustand'
 
+
+interface Store {
+    language: string // 'fr' | 'en'
+    setTranslation: (language: string) => void
+}
+
 const store = {
   language: "fr",
   setTranslation: (set: any) => (language: string) => set({ language }),
 }
 
-export const useBoundStore = create((set) => ({
+export const useBoundStore = create<Store>((set) => ({
     ...store,
     setTranslation: store.setTranslation(set),
   }));
